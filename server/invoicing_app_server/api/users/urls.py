81626b28a2012a6ce3fd7c3ab5f3cb66d62db128
Suyash Lawand <suyash.lawand@gmail.com>
2022-01-13 14:47:10 +0530

    added routes api-login api-logout & removed default from urls (root)

--- server/invoicing_app_server/api/users/urls.py
+++ server/invoicing_app_server/api/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import user, user_id
+from .views import user, user_id, api_login, api_logout
 
 urlpatterns = [
     path('', user),
-    path('<int:id>/', user_id)
+    path('<int:id>/', user_id),
+    path('api-login/', api_login),
+    path('api-logout/', api_logout),
 ]
--- server/invoicing_app_server/api/users/views.py
+++ server/invoicing_app_server/api/users/views.py
@@ -1,11 +1,15 @@
 import json
 import re
 
+from django.contrib.auth import login, logout
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import permission_classes, authentication_classes, api_view
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 from .models import CustomUser
 from django.views.decorators.csrf import csrf_exempt
@@ -31,7 +35,7 @@ def user(request):
             return JsonResponse({'err': 'Invalid email'}, status=401)
 
         if len(nu_data['password']) < 6:
-            return JsonResponse({'err': 'invalid password. Expected (< 6)'})
+            return JsonResponse({'err': 'invalid password. Expected (< 6)'}, status=401)
 
         nu_data['is_email_verified'] = False
         nu_data['is_phone_verified'] = False
@@ -83,3 +87,46 @@ def user_id(request: WSGIRequest, id: int):
         return JsonResponse({'err': str(e)})
 
     return JsonResponse(CustomUserSerializer(usr).data)
+
+
+@csrf_exempt
+def api_login(request: WSGIRequest):
+    usr_credentials = json.loads(request.body)
+
+    try:
+        usr = CustomUser.objects.get(email=usr_credentials['email'])
+
+        if usr.check_password(usr_credentials['password']):
+
+            # Delete Previous tokens.
+            for tkn in Token.objects.filter(user=usr):
+                tkn.delete()
+
+            token = Token.objects.create(user=usr)
+            login(request, user=usr)
+            return JsonResponse({'token': 'Token ' + token.key})
+        else:
+            return JsonResponse({'err': 'incorrect password.'}, status=401)
+
+    except Exception as e:
+        print(e)
+        return JsonResponse({'err': 'invalid email or password'}, status=400)
+
+
+@csrf_exempt
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def api_logout(request: WSGIRequest):
+
+    tkn = request.headers['Authorization'].split(' ')[-1]
+
+    try:
+        del_token = Token.objects.get(user=request.user, key=tkn)
+        del_token.delete()
+        logout(request)
+
+        return Response({'message': 'Logged out successfully.'})
+    except Exception as e:
+
+        return Response({'err': 'invalid user or authtoken'}, status=400)
