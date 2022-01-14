69777de5c5503e5e90a65ffd5eb623696630497c
Suyash Lawand <suyash.lawand@gmail.com>
2022-01-14 22:24:52 +0530

    removed default tokenauth routes

--- server/invoicing_app_server/invoicing_app_server/urls.py
+++ server/invoicing_app_server/invoicing_app_server/urls.py
@@ -9,24 +9,19 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URLConf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api-auth/', include('rest_framework.urls')),
-                  path('api-token-auth/', views.obtain_auth_token),  # TOKEN - AUTH Route
                   path('', include('api.urls'))
-
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
